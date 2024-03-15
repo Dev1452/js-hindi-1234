925e68572424d8b5c370ff107a09383c7f5d83b6
Devender jain <147976069+Dev1452@users.noreply.github.com>
2024-03-15 15:49:14 +0000

    variables , data-types , conversion

---
+++ 01_Basics/03_conversion.js
@@ -0,0 +1,93 @@
+                    //    My Practice
+
+let a = 33
+
+let b = String(a)
+// console.log(b);
+// console.log(typeof b);
+
+
+let number = "33"
+
+let strNumber = Number(number)
+// console.log(strNumber);
+// console.log(typeof strNumber);
+
+let boolean = false
+
+let numberBoolean = Number(boolean)
+// console.log(numberBoolean);
+// console.log(typeof numberBoolean);
+
+
+                //    Sir Practice
+
+let score = "hitesh"
+
+//console.log(typeof score);
+//console.log(typeof(score));
+
+let valueInNumber = Number(score)
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
+
+
+// "33" => 33
+// "33abc" => NaN
+//  true => 1; false => 0
+
+let isLoggedIn = "hitesh"
+
+let booleanIsLoggedIn = Boolean(isLoggedIn)
+//  console.log(booleanIsLoggedIn);
+
+// 1 => true; 0 => false
+// "" => false
+// "hitesh" => true
+
+let someNumber = 33
+
+let stringNumber = String(someNumber)
+// console.log(stringNumber);
+// console.log(typeof stringNumber);
+
+// *********************** Operations ***********************
+
+let value = 3
+let negValue = -value
+// console.log(negValue);
+
+// console.log(2+2);
+// console.log(2-2);
+// console.log(2*2);
+// console.log(2**3);
+// console.log(2/3);
+// console.log(2%3);
+
+let str1 = "hello"
+let str2 = " hitesh"
+
+let str3 = str1 + str2
+// console.log(str3);
+
+// console.log("1" + 2);
+// console.log(1 + "2");
+// console.log("1" + 2 + 2);
+// console.log(1 + 2 + "2");
+
+// console.log( (3 + 4) * 5 % 3);
+
+// console.log(+true);
+// console.log(+"");
+
+let num1, num2, num3
+
+num1 = num2 = num3 = 2 + 2
+
+let gameCounter = 100
+++gameCounter;
+console.log(gameCounter);
+
+// link to study
+// https://tc39.es/ecma262/multipage/abstract-operations.html#sec-type-conversion
+               
